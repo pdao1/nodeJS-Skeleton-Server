1b0ed16635b95d14d6a97bfb284a9996560d867b
pdao1 <69880265+pdao1@users.noreply.github.com>
2022-05-07 19:47:50 -1000

    added paths

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
     // request and response
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
   // set header content type
-  res.writeHead('200');
   res.setHeader('Content-Type', 'text/html');
 
-  res.write('<p>Sup hoe</p>');
-  res.end();
+  // pushing index
+  fs.readFile('./views/index.html', (err, data) => {
+  if (err) {
+    console.log(err);
+    res.end();
+  } else {
+    res.end(data);
+  }
 });
 
-server.listen(3000, 'localhost', () => {
+  server.listen(3000, 'localhost', () => {
   console.log('listen for requests on port 3000');
-});
+})});
