cf0cf55a4f76c78bab783f3c53df28f779178224
pdao1 <69880265+pdao1@users.noreply.github.com>
2022-05-09 22:48:23 -1000

    Additional server stuff

--- server.js
+++ server.js
@@ -8,16 +8,43 @@ const server = http.createServer((req, res) => {
   // set header content type
   res.setHeader('Content-Type', 'text/html');
 
+
+  // routing
+  let path = './views/';
+
+  switch(req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/contact':
+      path += 'contact-me.html';
+      res.statusCode = 200;
+      break;
+    case '/contact-me':
+      res.statusCode = 301;
+      res.setHeader('Location', '/contact'); // redirects
+      res.end();
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+      break;
+  }
+
   // pushing index
-  fs.readFile('./views/index.html', (err, data) => {
+  fs.readFile(path, (err, data) => {
   if (err) {
     console.log(err);
     res.end();
   } else {
-    res.write(data);
     res.end(data);
   }
-})
+  })
 });
 
   // server listen statement
