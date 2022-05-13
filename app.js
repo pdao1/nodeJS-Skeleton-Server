aff97ac81b6e4674a7e168b7d3e9f4a070fb68d4
pdao1 <69880265+pdao1@users.noreply.github.com>
2022-05-13 01:23:46 -1000

    bootstrap

--- app.js
+++ app.js
@@ -11,16 +11,29 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
 //  res.send('<h1>Hello</h1>');
-  res.sendFile('./views/index.html', {root: __dirname });
+  // res.sendFile('./views/index.html', {root: __dirname });
+  res.render('index');
 });
 app.get('/about', (req, res) => {
  // res.send('<p>About</p>');
-  res.sendFile('./views/about.html', {root: __dirname });
+  // res.sendFile('./views/about.html', {root: __dirname });
+  res.render('about')
 });
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get('/contact', (req, res) => {
+  res.render('contact')
 });
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create')
+});
+
+// redirect code
+// app.get('/about-us', (req, res) => {
+//   res.redirect('/about');
+// });
+
+
 // always make 4040 page last in order because it runs through all get requests to check if page exists.  If placed too high it will prematurely show 404 even if page exists.
-app.use((req, res) => {
-  res.sendFile('./views/404.html', { root: __dirname });
+app.get('404', (req, res) => {
+  res.render('404')
 });
